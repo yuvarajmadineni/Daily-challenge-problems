93ba6c0efe9dda7d3b99877ece95d4bc8db7b93e
abhinavdv <dvabhinav31@gmail.com>
2018-08-23 16:49:04 +0530

    Add files via upload

---
+++ 798A.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	int a = 0,i,b;
+	char str[15];
+	scanf("%s", str);
+	b=strlen(str);
+	if(b%2 != 0)
+		{for(i=0;i<b/2+1;i++)
+	{
+		if(str[i]!=str[b-i-1])
+		{
+			a++;
+		}
+	
+
+	}
+		}
+	else{
+		for(i=0;i<b/2;i++)
+	{
+		if(str[i]!=str[b-i-1])
+		{
+			a++;
+		}
+	
+
+
+
+	}
+}
+if(b%2 != 0 && a==0)
+	{
+		a++;
+	}
+
+	if(a==1 )
+	{
+		printf("YES");
+	}
+	else
+	{
+		printf("NO" );
+	}
+	return 0;
+}
